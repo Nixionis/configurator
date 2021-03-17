32fd1df27201f197d22a01de4497a0200c2c6c62
astyd256 <astyd637@gmail.com>
2021-03-17 19:51:37 +0700

    Creating base commit

--- Configurator/MyForm.h
+++ Configurator/MyForm.h
@@ -34,6 +34,18 @@ namespace Configurator {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::RadioButton^ radioButton1;
+	protected:
+	private: System::Windows::Forms::RadioButton^ radioButton2;
+	private: System::Windows::Forms::RadioButton^ radioButton3;
+	private: System::Windows::Forms::RadioButton^ radioButton4;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::ListBox^ listBox2;
 
 	private:
 		/// <summary>
@@ -48,12 +60,150 @@ namespace Configurator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->radioButton1 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton2 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton3 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton4 = (gcnew System::Windows::Forms::RadioButton());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->listBox2 = (gcnew System::Windows::Forms::ListBox());
+			this->groupBox1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// radioButton1
+			// 
+			this->radioButton1->AutoSize = true;
+			this->radioButton1->Location = System::Drawing::Point(29, 26);
+			this->radioButton1->Name = L"radioButton1";
+			this->radioButton1->Size = System::Drawing::Size(92, 21);
+			this->radioButton1->TabIndex = 1;
+			this->radioButton1->TabStop = true;
+			this->radioButton1->Text = L"Офисный";
+			this->radioButton1->UseVisualStyleBackColor = true;
+			this->radioButton1->CheckedChanged += gcnew System::EventHandler(this, &MyForm::radioButton1_CheckedChanged);
+			// 
+			// radioButton2
+			// 
+			this->radioButton2->AutoSize = true;
+			this->radioButton2->Location = System::Drawing::Point(29, 53);
+			this->radioButton2->Name = L"radioButton2";
+			this->radioButton2->Size = System::Drawing::Size(100, 21);
+			this->radioButton2->TabIndex = 2;
+			this->radioButton2->TabStop = true;
+			this->radioButton2->Text = L"Домашний";
+			this->radioButton2->UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this->radioButton3->AutoSize = true;
+			this->radioButton3->Location = System::Drawing::Point(29, 80);
+			this->radioButton3->Name = L"radioButton3";
+			this->radioButton3->Size = System::Drawing::Size(83, 21);
+			this->radioButton3->TabIndex = 3;
+			this->radioButton3->TabStop = true;
+			this->radioButton3->Text = L"Игровой";
+			this->radioButton3->UseVisualStyleBackColor = true;
+			// 
+			// radioButton4
+			// 
+			this->radioButton4->AutoSize = true;
+			this->radioButton4->Location = System::Drawing::Point(29, 107);
+			this->radioButton4->Name = L"radioButton4";
+			this->radioButton4->Size = System::Drawing::Size(219, 21);
+			this->radioButton4->TabIndex = 4;
+			this->radioButton4->TabStop = true;
+			this->radioButton4->Text = L"Про (для серьёзной работы)";
+			this->radioButton4->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(35, 12);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(26, 17);
+			this->label1->TabIndex = 6;
+			this->label1->Text = L"От";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(156, 12);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(27, 17);
+			this->label2->TabIndex = 7;
+			this->label2->Text = L"До";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(73, 12);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(77, 22);
+			this->textBox1->TabIndex = 8;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(189, 12);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(81, 22);
+			this->textBox2->TabIndex = 9;
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 16;
+			this->listBox1->Location = System::Drawing::Point(351, 11);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(127, 180);
+			this->listBox1->TabIndex = 10;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->radioButton4);
+			this->groupBox1->Controls->Add(this->radioButton3);
+			this->groupBox1->Controls->Add(this->radioButton2);
+			this->groupBox1->Controls->Add(this->radioButton1);
+			this->groupBox1->Location = System::Drawing::Point(38, 54);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(260, 137);
+			this->groupBox1->TabIndex = 11;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"groupBox1";
+			// 
+			// listBox2
+			// 
+			this->listBox2->FormattingEnabled = true;
+			this->listBox2->ItemHeight = 16;
+			this->listBox2->Location = System::Drawing::Point(529, 11);
+			this->listBox2->Name = L"listBox2";
+			this->listBox2->Size = System::Drawing::Size(120, 180);
+			this->listBox2->TabIndex = 12;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(713, 221);
+			this->Controls->Add(this->listBox2);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void radioButton1_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
